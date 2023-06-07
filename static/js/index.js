27307552358892bea1ca56493cc5753d5b1f34e9
Brian <bstanton773@gmail.com>
2023-06-07 16:46:28 -0500

    Handle Form Submit to log brewery data to console

--- static/js/index.js
+++ static/js/index.js
@@ -10,10 +10,15 @@ function pageLoader(){
         btn.addEventListener('click', changeBackgroundColor);
     };
 
+    // Add the changeDiv function to the Nav Links
     const navLinks = document.getElementsByClassName('nav-link')
     for (let link of navLinks){
         link.addEventListener('click', changeDiv)
     }
+
+    // Add the brew finder when form submits
+    const findBrewsForm = document.querySelector('#find-brews-form');
+    findBrewsForm.addEventListener('submit', e => findBreweries(e, 1));
 
 }
 
@@ -46,4 +51,21 @@ function changeDiv(e){
     const toTurnOn = document.getElementById(toTurnOnID);
     toTurnOn.classList.replace('is-invisible', 'is-visible');
     e.target.classList.add('active')
+}
+
+
+// Function that will run API call from user Input to get breweries
+function findBreweries(event, page){
+    event.preventDefault();
+
+    console.log('Finding breweries...');
+    const city = document.getElementsByName('city')[0].value;
+    const perPage = 10;
+    const url = `https://api.openbrewerydb.org/v1/breweries?by_city=${city}&per_page=${perPage}&page=${page}`
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
 }
